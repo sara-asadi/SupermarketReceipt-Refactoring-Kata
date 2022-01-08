92077e17008b9ae4531188280265a3183c957f0d
Sara Asadi <49518920+sara-asadi@users.noreply.github.com>
2022-01-08 20:47:34 +0330

    "offersHandlerPerItem" added + for handling each offer, an specific method added to class "Offer" + "getProduct" deleted from class "offer" in case of being unused

--- java/src/main/java/dojo/supermarket/model/Offer.java
+++ java/src/main/java/dojo/supermarket/model/Offer.java
@@ -11,8 +11,44 @@ public class Offer {
         this.product = product;
     }
 
-    Product getProduct() {
-        return this.product;
+    public Discount handleTenPercentDiscount(Product p, double quantity, double unitPrice) {
+        Discount discount;
+        discount = new Discount(p, this.argument + "% off", -quantity * unitPrice * this.argument / 100.0);
+        return discount;
+    }
+
+    public Discount handleFiveForAmount(Product p, double quantity, double unitPrice) {
+        int offerPackSize = 5;
+        int numberOfPacks = ((int) quantity) / offerPackSize;
+        if (((int) quantity) >= 5) {
+            double discountTotal = unitPrice * quantity - (this.argument * numberOfPacks + ((int) quantity) % 5 * unitPrice);
+            return new Discount(p, offerPackSize + " for " + this.argument, -discountTotal);
+        }
+        return null;
+    }
+
+    public Discount handleTwoForAmount(Product p, double quantity, double unitPrice) {
+        int offerPackSize = 2;
+
+        if (((int) quantity) >= 2) {
+            int intDivision = ((int) quantity) / offerPackSize;
+            double pricePerUnit = this.argument * intDivision;
+            double theTotal = (((int) quantity) % 2) * unitPrice;
+            double total = pricePerUnit + theTotal;
+            double discountN = unitPrice * quantity - total;
+            return new Discount(p, "2 for " + this.argument, -discountN);
+        }
+        return null;
+    }
+
+    public Discount handleThreeForTwo(Product p, double quantity, double unitPrice) {
+        int offerPackSize = 3;
+        int numberOfPacks = ((int) quantity) / offerPackSize;
+        if (((int) quantity) > 2) {
+            double discountAmount = quantity * unitPrice - ((numberOfPacks * 2 * unitPrice) + ((int) quantity) % 3 * unitPrice);
+            return new Discount(p, "3 for 2", -discountAmount);
+        }
+        return null;
     }
 
 }
--- java/src/main/java/dojo/supermarket/model/Teller.java
+++ java/src/main/java/dojo/supermarket/model/Teller.java
@@ -28,45 +28,26 @@ public class Teller {
         for (Product p: productQuantities.keySet()) {
             double quantity = productQuantities.get(p);
             if (this.offers.containsKey(p)) {
-                Offer offer = this.offers.get(p);
-                double unitPrice = this.catalog.getUnitPrice(p);
-                int quantityAsInt = (int) quantity;
-                Discount discount = null;
-                int x = 1;
-                if (offer.offerType == SpecialOfferType.ThreeForTwo) {
-                    x = 3;
-
-                } else if (offer.offerType == SpecialOfferType.TwoForAmount) {
-                    x = 2;
-                    if (quantityAsInt >= 2) {
-                        int intDivision = quantityAsInt / x;
-                        double pricePerUnit = offer.argument * intDivision;
-                        double theTotal = (quantityAsInt % 2) * unitPrice;
-                        double total = pricePerUnit + theTotal;
-                        double discountN = unitPrice * quantity - total;
-                        discount = new Discount(p, "2 for " + offer.argument, -discountN);
-                    }
-
-                } if (offer.offerType == SpecialOfferType.FiveForAmount) {
-                    x = 5;
-                }
-                int numberOfXs = quantityAsInt / x;
-                if (offer.offerType == SpecialOfferType.ThreeForTwo && quantityAsInt > 2) {
-                    double discountAmount = quantity * unitPrice - ((numberOfXs * 2 * unitPrice) + quantityAsInt % 3 * unitPrice);
-                    discount = new Discount(p, "3 for 2", -discountAmount);
-                }
-                if (offer.offerType == SpecialOfferType.TenPercentDiscount) {
-                    discount = new Discount(p, offer.argument + "% off", -quantity * unitPrice * offer.argument / 100.0);
-                }
-                if (offer.offerType == SpecialOfferType.FiveForAmount && quantityAsInt >= 5) {
-                    double discountTotal = unitPrice * quantity - (offer.argument * numberOfXs + quantityAsInt % 5 * unitPrice);
-                    discount = new Discount(p, x + " for " + offer.argument, -discountTotal);
-                }
-                if (discount != null)
-                    this.receipt.addDiscount(discount);
+                offersHandlerPerItem(p, quantity);
             }
-
         }
+    }
+
+    private void offersHandlerPerItem(Product p, double quantity) {
+        Offer offer = this.offers.get(p);
+        double unitPrice = this.catalog.getUnitPrice(p);
+        Discount discount = null;
+
+        if (offer.offerType == SpecialOfferType.ThreeForTwo)
+            discount = offer.handleThreeForTwo(p, quantity, unitPrice);
+        else if (offer.offerType == SpecialOfferType.TwoForAmount)
+            discount = offer.handleTwoForAmount(p, quantity, unitPrice);
+        if (offer.offerType == SpecialOfferType.FiveForAmount)
+            discount = offer.handleFiveForAmount(p, quantity, unitPrice);
+        if (offer.offerType == SpecialOfferType.TenPercentDiscount)
+            discount = offer.handleTenPercentDiscount(p, quantity, unitPrice);
+        if (discount != null)
+            this.receipt.addDiscount(discount);
     }
 
     private void getReceipt(ShoppingCart theCart) {
