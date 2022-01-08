2d4113aaec9e0695240b69ac43ee9e201c8e413b
Sara Asadi <49518920+sara-asadi@users.noreply.github.com>
2022-01-08 19:48:41 +0330

    "handleOffers" moved from "ShoppingCart" to "Teller"

--- java/src/main/java/dojo/supermarket/model/Teller.java
+++ java/src/main/java/dojo/supermarket/model/Teller.java
@@ -8,7 +8,7 @@ public class Teller {
 
     private final SupermarketCatalog catalog;
     private Map<Product, Offer> offers = new HashMap<>();
-
+    private Receipt receipt;
     public Teller(SupermarketCatalog catalog) {
         this.catalog = catalog;
     }
@@ -18,23 +18,67 @@ public class Teller {
     }
 
     public Receipt checksOutArticlesFrom(ShoppingCart theCart) {
-        Receipt receipt = getReceipt(theCart);
-        theCart.handleOffers(receipt, this.offers, this.catalog);
+        getReceipt(theCart);
+        handleOffers(theCart.getProductQuantities());
 
-        return receipt;
+        return this.receipt;
     }
 
-    private Receipt getReceipt(ShoppingCart theCart) {
-        Receipt receipt = new Receipt();
+    private void handleOffers(Map<Product, Double> productQuantities) {
+        for (Product p: productQuantities.keySet()) {
+            double quantity = productQuantities.get(p);
+            if (this.offers.containsKey(p)) {
+                Offer offer = this.offers.get(p);
+                double unitPrice = this.catalog.getUnitPrice(p);
+                int quantityAsInt = (int) quantity;
+                Discount discount = null;
+                int x = 1;
+                if (offer.offerType == SpecialOfferType.ThreeForTwo) {
+                    x = 3;
+
+                } else if (offer.offerType == SpecialOfferType.TwoForAmount) {
+                    x = 2;
+                    if (quantityAsInt >= 2) {
+                        int intDivision = quantityAsInt / x;
+                        double pricePerUnit = offer.argument * intDivision;
+                        double theTotal = (quantityAsInt % 2) * unitPrice;
+                        double total = pricePerUnit + theTotal;
+                        double discountN = unitPrice * quantity - total;
+                        discount = new Discount(p, "2 for " + offer.argument, -discountN);
+                    }
+
+                } if (offer.offerType == SpecialOfferType.FiveForAmount) {
+                    x = 5;
+                }
+                int numberOfXs = quantityAsInt / x;
+                if (offer.offerType == SpecialOfferType.ThreeForTwo && quantityAsInt > 2) {
+                    double discountAmount = quantity * unitPrice - ((numberOfXs * 2 * unitPrice) + quantityAsInt % 3 * unitPrice);
+                    discount = new Discount(p, "3 for 2", -discountAmount);
+                }
+                if (offer.offerType == SpecialOfferType.TenPercentDiscount) {
+                    discount = new Discount(p, offer.argument + "% off", -quantity * unitPrice * offer.argument / 100.0);
+                }
+                if (offer.offerType == SpecialOfferType.FiveForAmount && quantityAsInt >= 5) {
+                    double discountTotal = unitPrice * quantity - (offer.argument * numberOfXs + quantityAsInt % 5 * unitPrice);
+                    discount = new Discount(p, x + " for " + offer.argument, -discountTotal);
+                }
+                if (discount != null)
+                    this.receipt.addDiscount(discount);
+            }
+
+        }
+    }
+
+    private void getReceipt(ShoppingCart theCart) {
+        this.receipt = new Receipt();
         List<ProductQuantity> productQuantities = theCart.getItems();
         for (ProductQuantity pq: productQuantities) {
             Product p = pq.getProduct();
             double quantity = pq.getQuantity();
             double unitPrice = this.catalog.getUnitPrice(p);
             double price = quantity * unitPrice;
-            receipt.addProduct(p, quantity, unitPrice, price);
+            this.receipt.addProduct(p, quantity, unitPrice, price);
         }
-        return receipt;
     }
 
 }
